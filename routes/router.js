178cdc01db79b9700bd31824068ef2e0caa14888
Claudia Villarroel <claudia.villarroel@live.com>
2024-01-21 16:32:55 -0300

    :hammer: Implementación logica solitada y mejoras

--- routes/router.js
+++ routes/router.js
@@ -1,8 +1,47 @@
 const express = require('express');
 const router = express.Router();
+const fs = require('fs');
 
 router.get('/', (req, res) => {
-    res.send('Hello World!');
-})
+  res.sendFile('index.html', { root: '.' });
+});
+
+router.get('/canciones', (req, res) => {
+  const canciones = JSON.parse(fs.readFileSync('./repertorio.json', 'utf-8'));
+  res.send(canciones);
+});
+
+router.post('/canciones', (req, res) => {
+  const cancion = req.body;
+  if(!cancion.titulo && !cancion.artista && !cancion.tono) {
+    return res.status(400).send('Se requiere a lo menos un campo');
+  }
+  const canciones = JSON.parse(fs.readFileSync('./repertorio.json', 'utf-8'));
+  canciones.push(cancion);
+  fs.writeFileSync('./repertorio.json', JSON.stringify(canciones));
+  res.send('Canción agregada');
+});
+
+router.put('/canciones/:id', (req, res) => {
+  const { id } = req.params;
+  const cancion = req.body;
+  if(!cancion.titulo && !cancion.artista && !cancion.tono) {
+    return res.status(400).send('Se requiere a lo menos un campo');
+  }
+  const canciones = JSON.parse(fs.readFileSync('./repertorio.json', 'utf-8'));
+  const index = canciones.findIndex((c) => c.id == id);
+  canciones[index] = cancion;
+  fs.writeFileSync('./repertorio.json', JSON.stringify(canciones));
+  res.send('Canción actualizada');
+});
+
+router.delete('/canciones/:id', (req, res) => {
+  const { id } = req.params;
+  const canciones = JSON.parse(fs.readFileSync('./repertorio.json', 'utf-8'));
+  const index = canciones.findIndex((c) => c.id == id);
+  canciones.splice(index, 1);
+  fs.writeFileSync('./repertorio.json', JSON.stringify(canciones));
+  res.send('Canción eliminada');
+});
 
 module.exports = router;
